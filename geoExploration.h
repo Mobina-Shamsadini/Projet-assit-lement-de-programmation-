e6098fc98d8e4bb99275a78983d8ec3360327f69
Mobina-Shamsadini <msmotlagh@etu.uqac.ca>
2025-12-14 02:08:46 -0500

    Delete geoExploration.h

--- geoExploration.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "geoQuiz.h"
-#include <string>
-
-double convertisseur(int montant = 100, std::string devise = "AED");
-void geoExploration(Profil test);
-void voyager(std::string* voyages, int nbEtapes);
